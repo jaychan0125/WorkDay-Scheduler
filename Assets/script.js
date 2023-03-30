599f51783b65fcd332d0b11b2dbd06d57c60fc06
jaychan0125 <joycechan0125@gmail.com>
2023-03-30 13:48:05 -0400

    question about console.log function

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,14 @@
+var saveBtn = $('.saveBtn')  //will return an array/nodelist
+var userSchedule = $('.description') //will return an array/nodelist
+console.log(saveBtn)
+console.log(saveBtn.parent('div').attr('id'))
+console.log(userSchedule)
+console.log(userSchedule.parent('div').attr('id'))
+
+saveBtn.on('click',function() {
+  console.log(this.parent('div').attr('id'))
+})
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -9,15 +20,55 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+// saveBtn.on('click',function() {
+//   console.log(this.parent('div').attr('id'))
+// })
+
+
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+
+
+
+
+
+
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
+
+
+
+
+
+
+
+
+
   // TODO: Add code to display the current date in the header of the page.
+
+
+
+
+
+
+
+
+
+
 });
---
+++ READEMEnotes.md
@@ -0,0 +1,18 @@
+Problem: create a task planner for the work day
+
+- display date on top
+- ^dynamically changes
+
+- set hours: 9am - 5pm (workday hours)
+    - past hours gray out
+    - current hour is red/yellow
+    - upcoming hours are green
+- ^dynamically change with time 
+
+- saveBtn allows user to save their input
+    - save into local storage -> when refresh, will stay
+
+- (notes clear upon new day)
+
+
+
